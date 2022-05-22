9dedcb443bda1ce6afe211c69a4497ba9bf2169e
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2022-05-22 09:49:42 +0200

    add B source

---
+++ Source/rADWandlerController.m
@@ -0,0 +1,28 @@
+#import "USBWindowController.h"
+
+
+/*
+@implementation IOWarriorWindowController(rADWandlerController)
+- (void)Alert:(NSString*)derFehler
+{
+   NSAlert * DebugAlert = [NSAlert init];
+   DebugAlert.messageText = @"Debugger!";
+   DebugAlert.informativeText = [NSString stringWithFormat:@"Mitteilung: \n%@",derFehler ];
+   
+   [DebugAlert  addButtonWithTitle:@"OK"];
+   [DebugAlert runModal];
+
+}
+
+- (void)drawRect:(NSRect)rect 
+{
+NSLog(@"drawRect");
+    // Drawing code here.
+}
+
+- (void)MehrkanalDatenPanelDidEnd:(NSSavePanel *)sheet returnCode:(int)returnCode  contextInfo:(void  *)contextInfo;
+{
+NSLog(@"MehrkanalDatenPanelDidEnd ret code: %d",returnCode);
+}
+@end
+*/
